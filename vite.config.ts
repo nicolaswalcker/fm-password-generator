86c597cf83230d9e606f267c7008376d8ad48dd1
Nicolas Walcker de Miranda <walcker_@outlook.com>
2023-01-24 21:05:14 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/assets/sass/_breakpoints.scss";
+        @import "./src/assets/sass/_colors.scss";
+        @import "./src/assets/sass/_normalize.scss";
+        @import "./src/assets/sass/_variables.scss";
+        @import "./src/assets/sass/_global.scss";
+        `,
+      },
+    },
+  },
+  plugins: [vue()],
+})
