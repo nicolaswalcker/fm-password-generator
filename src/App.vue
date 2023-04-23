17292159a195e7d9f34371f46b72e8eb639474c0
Nicolas <walcker_@outlook.com>
2023-04-23 18:17:29 -0300

    feat(title): adds app title

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,26 @@
 </script>
 
 <template>
-  <div class="main"><PasswordGenerator /></div>
+  <div class="main">
+    <h3 class="title">Password Generator</h3>
+    <PasswordGenerator />
+  </div>
 </template>
 
 <style lang="scss">
   .main {
     display: flex;
+    flex-direction: column;
+    gap: 2rem;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: 100%;
+  }
+
+  .title {
+    font-size: 1.5rem;
+    color: $grey-100;
+    text-align: center;
   }
 </style>
